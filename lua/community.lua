c0ce9c401dc721840683b637429e23f4ea461c32
Andi Hafner <me@example.com>
2026-05-25 19:54:44 +0200

    Remap leader e key from neo-tree to nvim-tree file explorer

--- lua/community.lua
+++ lua/community.lua
@@ -10,20 +10,35 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function() require("nvim-tree").setup {} end,
   },
   {
     "AstroNvim/astroui",
     opts = {
       colorscheme = "solarized",
+    },
+  },
+  -- ahc remap leader e key from neo-tree to nvim-tree file explorer
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>e", false },
+    },
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    keys = {
+      {
+        "<leader>e",
+        function() require("nvim-tree.api").tree.toggle() end,
+        desc = "Toggle Explorer",
+      },
     },
   },
 }
